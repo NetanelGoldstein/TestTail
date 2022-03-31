0eea66ce9c1aaa93ef11140f30c440491e7e43be
Oliver James <olliejames12@gmail.com>
2022-03-31 19:38:17 -0400

    Revert "Hope You can't change this"
    This reverts commit c5d3656f2f72f9051c5aa539a0dd17d230377268.

--- Test/Assets/Scripts/MovePlayer.cs
+++ Test/Assets/Scripts/MovePlayer.cs
@@ -10,7 +10,7 @@ public class MovePlayer : MonoBehaviour
     Rigidbody2D _rb;
     public Rigidbody2D jumpRB;
 
-    float _playerSpeed = 8f;
+    float _playerSpeed = 7f;
 
     //when we grab input using horizontal - means going to left, and positive number means right
     float _inputHorizontal;
