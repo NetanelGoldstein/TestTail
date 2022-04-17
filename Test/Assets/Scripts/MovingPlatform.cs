b8c2e7b29f00786a135cd3e63503d1881bdbc61c
Oliver James <olliejames12@gmail.com>
2022-04-17 18:41:46 -0400

    Fox versus a donkey

--- Test/Assets/Scripts/MovingPlatform.cs
+++ Test/Assets/Scripts/MovingPlatform.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
 
-
-
+    /**
+     * 
+     *with help from tutorials from https://www.youtube.com/c/Antarsoft
+    * platforms move left and right , timed interval
+    * **/
     public List<Transform> points;
     int goalPoint = 0;
     public Transform platform;
@@ -33,45 +36,8 @@ public class MovingPlatform : MonoBehaviour
             else goalPoint++;
             
 
-            //Check if we reached the last point, reset to first point
-            //0 1 2 3 (total 4)
-
-
 
         }
     }
 
 }
-/**
-   public float speed = 2f; //speed of the platform
-   public int startingPoint; //starting index (position of the platform)
-   public Transform[] points; //An array of transfomr points (positions wehere the platform needs to move)
-
-   private int i; //index of the array
-
-
-
-   // Start is called before the first frame update
-   void Start()
-   {
-       transform.position = points[startingPoint].position; //Settting the position of the platform to
-                                                           //the position of the points using index "startingPoint"
-   }
-
-   // Update is called once per frame
-   void Update()
-   {
-
-
-       if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
-       {
-           i++;
-           if (i == points.length) //check if the platform was on teh last point after the index increase
-           {
-               i = 0; //reset the index
-           }
-       }
-       //Moving the platform to the point position with the index "i"
-       transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
-
-   } **/
